bbd1c256cd5214f699c0711ed5a645cc27136f99
Daniel Ugulino <danielugulino46@gmail.com>
2021-11-08 23:38:31 -0300

    alterações de design e futuro floatlayout

--- design.py
+++ design.py
@@ -1,47 +1,35 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from insta_downnloader import insta_D
 
-class MyGrid(Widget): 
+# https://www.techwithtim.net/tutorials/kivy-tutorial/floatlayout/
+
+class LoginScreen(Screen): 
     user = ObjectProperty(None)
     key = ObjectProperty(None)
     
     def Login(self):
         user = self.user.text
         key = self.key.text
-        sla = insta_D.login(user,key)
+        sla = insta_D.Login(user,key)
         print(sla)
         return(sla)
 
-    # def build(self):
-    #     self.window = GridLayout()  
-    #     self.window.cols = 1
+class MenuScreen(Screen):
+    pass
 
-    #     self.window.add_widget(Image(source="stollker logo.png"))
-    #     self.Llogin = Label(text = "Insira seu usuario:")
-    #     self.user_text = TextInput(multiline = False)
-    #     self.window.add_widget(self.Llogin)
-    #     self.window.add_widget(self.user_text)
+class WindowManager(ScreenManager):
+    pass
 
-    #     self.Lkey = Label(text = "Insira sua senha:")
-    #     self.key_text = TextInput(multiline = False)
-    #     self.window.add_widget(self.Lkey)
-    #     self.window.add_widget(self.key_text)
-
-    #     self.Login_button = Button(text = "Logar")
-    #     self.Login_button.bind(on_press = self.Login)
-
-    #     self.window.add_widget(self.Login_button)
-        
-    #     return self.window
+kv = Builder.load_file("my.kv")
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
-        # self.window.add_widget(Image(source="stollker logo.png"))
-        # return self.window
+        return kv
 
 
 if __name__ == "__main__":

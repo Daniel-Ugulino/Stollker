8114032595780781407af7c42862b9db2f2a8298
Daniel Ugulino <danielugulino46@gmail.com>
2021-11-12 00:29:37 -0300

    alterações no funcionamento do campo de data e popup de erro

--- design.py
+++ design.py
@@ -2,16 +2,36 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.picker import MDDatePicker
 from insta_downnloader import insta_D
 from PIL import Image
 import easygui
+from googletrans import Translator
 
 # https://www.techwithtim.net/tutorials/kivy-tutorial/floatlayout/
+def show_alert_dialog(self,erro):
+    
+    # sla = self.dialog.dismiss(force=True)
+    if not self.dialog:
+        self.dialog = MDDialog(
+            text=str(erro),
+            buttons=[
+                MDFlatButton(
+                    text="SAIR", on_release = self.close_dialog,
+                ),
+            ],
+            )
+    self.dialog.open()
+
 
 
 class LoginScreen(Screen):
+    def close_dialog(self,erro):
+        self.dialog.dismiss()
+
+    dialog = None
     user = ObjectProperty(None)
     key = ObjectProperty(None)
 
@@ -22,9 +42,15 @@ class LoginScreen(Screen):
         if (login == True):
             sm.current = "input"
         else:
-            print(login)
+            # retorno = login
+            # error = Translator.translate(str(retorno), dest="pt-br")
+            show_alert_dialog(self,login)
 
 class InputScreen(Screen):
+    def close_dialog(self,erro):
+        self.dialog.dismiss()
+
+    dialog = None
     hashtag = ObjectProperty(None)
     dia = ObjectProperty(None)
     qtd = ObjectProperty(None)
@@ -35,7 +61,6 @@ class InputScreen(Screen):
 
     def on_save(self,instance, value, date_range):
         day = value.strftime("%d/%m/%Y")
-        print(type(day))
         self.dia.text = day
         
     def show_time_picker(self):
@@ -46,11 +71,15 @@ class InputScreen(Screen):
     def ImagesPicker(self):
         hashtag = self.hashtag.text
         qtd = self.qtd.text
-        get_images = insta_D.ByHashtag(str(hashtag), int(qtd))
+        dia = self.dia.text
+        if dia == "Insira a data da publicação":
+            dia = ""
+        get_images = insta_D.ByHashtag(str(hashtag), int(qtd), dia)
         if(get_images == True):
             sm.current = "images"
         else:
-            print(get_images)
+            # error = Translator.translate(str(get_images), dest="pt-br")
+            show_alert_dialog(self,get_images)
 
     def FileInput(self):
         file = easygui.fileopenbox()
@@ -59,8 +88,7 @@ class InputScreen(Screen):
             for i in range(len(file_nameC)):
                 file_name = file_nameC[i]
             self.file_text.text = file_name
-            type = file_name.rsplit('.')
-            print(type[1])
+            # type = file_name.rsplit('.')
             banner = Image.open(file)
             banner = banner.convert("RGB")
             banner = banner.save("banner.jpg")
@@ -90,7 +118,7 @@ class MyApp(MDApp):
             name="input"), ImagesScreen(name="images")]
         for screen in screens:
             sm.add_widget(screen)
-        sm.current = "input"
+        sm.current = "login"
         return sm
 
 if __name__ == "__main__":
--- insta_downnloader.py
+++ insta_downnloader.py
@@ -2,42 +2,72 @@ import os
 from instagrapi import Client
 import win32print
 import win32api
+from datetime import datetime
 
 cl = Client()
 
-class insta_D():  
-    def Login(user,key):
+
+class insta_D():
+    def Login(user, key):
         try:
             login = cl.login(user, key)
             return(True)
         except Exception as e:
             return(e)
 
-    def ByHashtag(hashtags,qtd): 
+    def ByHashtag(hashtags, qtd, day_selected=""):
+       
+        # try:
+        #     data = cl.hashtag_medias_recent(hashtags, amount="")
 
-        # hashtag1 = cl.hashtag_info(hashtags)
-        # qtd = hashtag1.dict()
-        # qtd = int(qtd["media_count"])
-        
+        # except:
         # hashtags = input("Insira a hashtag desejada:")
         # qtd = int(input("Insira a quantidade de posts desejados:"))
-        print("Coletando imagens")
+
+        # print("Coletando imagens")
         urls = []
         try:
-            data = cl.hashtag_medias_recent(hashtags, amount=qtd)
-
+            print("Coletando dados")
+            hashtag1 = cl.hashtag_info(hashtags)
+            qtd_total = hashtag1.dict()
+            qtd_total = int(qtd_total["media_count"])
+            if (int(qtd_total) >= 500):
+                print("cu")
+                qtd_total = 500
+            print("Coletando imagens")
+            print(qtd_total)
+            data = cl.hashtag_medias_recent(hashtags, amount=qtd_total)
+            
             for i in range(len(data)):
+                print(i)
                 data_obj = data[i].dict()
-                data_url = str(data_obj["thumbnail_url"])
-                if(data_url != "None"):
-                    data_url = data_url.replace("HttpUrl(", "")
-                    data_url = data_url.replace("'", "")
-                    urls.append(data_url)
-                elif(data_url == "None"):
-                    res = data_obj["resources"]
-                    for j in range(len(res)):
-                        res1 = res[j]
-                        urls.append(str(res1["thumbnail_url"]))
+                if (day_selected != ""):
+                    day_taken = data_obj["taken_at"].date()
+                    day_taken = day_taken.strftime("%d/%m/%Y")
+                    print(day_taken,day_selected)
+                    if(day_selected == day_taken):
+                        data_url = str(data_obj["thumbnail_url"])
+                        if(data_url != "None"):
+                            data_url = data_url.replace("HttpUrl(", "")
+                            data_url = data_url.replace("'", "")
+                            urls.append(data_url)
+                            
+                        elif(data_url == "None"):
+                            res = data_obj["resources"]
+                            for j in range(len(res)):
+                                res1 = res[j]
+                                urls.append(str(res1["thumbnail_url"]))
+                else:
+                    data_url = str(data_obj["thumbnail_url"])
+                    if(data_url != "None"):
+                        data_url = data_url.replace("HttpUrl(", "")
+                        data_url = data_url.replace("'", "")
+                        urls.append(data_url)
+                    elif(data_url == "None"):
+                        res = data_obj["resources"]
+                        for j in range(len(res)):
+                            res1 = res[j]
+                            urls.append(str(res1["thumbnail_url"]))
 
             # count = 0
             photo_folder = "./temp_" + hashtags
@@ -46,18 +76,16 @@ class insta_D():
                 # count += 1
                 # photo_folder = "./temp_" + str(count) + "_" + hashtags
                 os.makedirs(photo_folder)
-
-
-            for i in range(len(urls)):
+            
+            for i in range(qtd):
                 cl.photo_download_by_url(url=urls[i], filename=(
                     hashtags + "_fotos_" + str(i + 1)), folder=photo_folder)
                 print(f"Imagem {i+1} da {hashtags} baixada")
-            
+
             return(True)
 
         except Exception as e:
-            print(e)
-
+            return(e)
 
     def Print_out():
 
@@ -74,6 +102,4 @@ class insta_D():
         # win32print.WritePrinter(prt, img)
         # win32print.EndDocPrinter(prt)
         # win32print.ClosePrinter(prt)
-
-
 

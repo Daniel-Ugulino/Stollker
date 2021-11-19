68f28a0d8b6949e4367d60f19d3ebd86bd3f6d6b
Sky3ds <danielugulino46@gmail.com>
2021-11-19 02:49:36 -0300

    Correção de bugs e alterações finais da versão 1.0

--- insta_downnloader.py
+++ insta_downnloader.py
@@ -1,7 +1,5 @@
 import os
 from instagrapi import Client
-import win32print
-import win32api
 from datetime import date, datetime
 
 cl = Client()
@@ -17,14 +15,6 @@ class insta_D():
 
     def ByHashtag(hashtags, qtd, day_selected=""):
        
-        # try:
-        #     data = cl.hashtag_medias_recent(hashtags, amount="")
-
-        # except:
-        # hashtags = input("Insira a hashtag desejada:")
-        # qtd = int(input("Insira a quantidade de posts desejados:"))
-
-        # print("Coletando imagens")
         urls = []
         dates = []
         try:
@@ -32,12 +22,10 @@ class insta_D():
             hashtag1 = cl.hashtag_info(hashtags)
             qtd_total = hashtag1.dict()
             qtd_total = int(qtd_total["media_count"])
-            if (int(qtd_total) >= 500):
-                qtd_total = 500
-               
+
             print("Coletando imagens")
          
-            data = cl.hashtag_medias_recent(hashtags, amount=qtd_total)
+            data = cl.hashtag_medias_recent(hashtags, amount=500)
             
             for i in range(len(data)):
                 # print(i)
@@ -49,75 +37,75 @@ class insta_D():
                     day_taken_formated = day_taken_formated.strftime("%d/%m/%Y")
                     # print(day_taken,day_selected)
                     if(day_selected == day_taken_formated):
+
                         data_url = str(data_obj["thumbnail_url"])
                         if(data_url != "None"):
                             data_url = data_url.replace("HttpUrl(", "")
                             data_url = data_url.replace("'", "")
                             urls.append(data_url)
-                            dates.append(day_taken)
+                            # dates.append(day_taken)
                             
                         elif(data_url == "None"):
                             res = data_obj["resources"]
                             for j in range(len(res)):
                                 res1 = res[j]
                                 urls.append(str(res1["thumbnail_url"]))
-                                dates.append(day_taken)
+                                # dates.append(day_taken)
                 else:
                     data_url = str(data_obj["thumbnail_url"])
+                    
                     if(data_url != "None"):
                         data_url = data_url.replace("HttpUrl(", "")
                         data_url = data_url.replace("'", "")
                         urls.append(data_url)
-                        dates.append(day_taken)
+                        # dates.append(day_taken)
+
                     elif(data_url == "None"):
                         res = data_obj["resources"]
                         for j in range(len(res)):
                             res1 = res[j]
                             urls.append(str(res1["thumbnail_url"]))
-                            dates.append(day_taken)
+                            # dates.append(day_taken)
 
-            # count = 0
             photo_folder = "./temp_" + hashtags
 
             if(os.path.isdir(photo_folder) == False):
-                # count += 1
-                # photo_folder = "./temp_" + str(count) + "_" + hashtags
                 os.makedirs(photo_folder)
-
-            if (qtd > qtd_total): qtd = qtd_total
-
-            if (qtd > len(urls) ): qtd = len(urls)
-
-            # print(qtd)
-            # print(len(dates))
-            # print(len(urls))
-
-            for i in range(qtd):
-                dates_formated = str(dates[i])
-                dates_formated = dates_formated.replace(':',"_")
-                dates_formated = dates_formated.replace(' ',"_")
-                dates_formated = dates_formated.replace('+',"_")
+            print(type(qtd))
+            print(len(urls))
+            if(qtd == ""): qtd = len(urls)
+            elif(int(qtd == 0)): qtd = len(urls)
+            elif (int(qtd) > len(urls)): qtd = len(urls)
+            
+            # dates_formated_before = " "
+            for i in range(int(qtd)):
+                # dates_formated = str(dates[i])
+                # dates_formated = dates_formated.replace(':',"_")
+                # dates_formated = dates_formated.replace(' ',"_")
+                # dates_formated = dates_formated.replace('+',"_")
+                
+                # if(str(dates_formated) == dates_formated_before):
+                #     dates_formated = str(dates_formated + "_" + str[i])
 
                 cl.photo_download_by_url(url=urls[i], filename= (
-                    hashtags + "_fotos_" + str(dates_formated) ), folder=photo_folder)
-                # print(f"Imagem {i+1} da {hashtags} baixada")
-
+                    hashtags + "_fotos_" + str(i)), folder=photo_folder)
+                # dates_formated_before = dates_formated
             return(True)
 
         except Exception as e:
             return(e)
 
-    def Print_out():
+    # def Print_out():
 
         # print("Imprimindo imagem")
 
         # img = Image.open('./temp_1_brasileirao/brasileirao_fotos_1.jpg')
-        img = "brasileirao_fotos_2.jpg"
-        caminho = "./temp_brasileirao/"
-        defprt = win32print.GetDefaultPrinter()
-        prt = win32print.SetDefaultPrinter(defprt)
+        # img = "brasileirao_fotos_2.jpg"
+        # caminho = "./temp_brasileirao/"
+        # defprt = win32print.GetDefaultPrinter()
+        # prt = win32print.SetDefaultPrinter(defprt)
 
-        win32api.ShellExecute(0, "print", img, None, caminho, 0)
+        # win32api.ShellExecute(0, "print", img, None, caminho, 0)
         # win32print.StartDocPrinter(prt, 1, ("Imagem Instagram", None, None))
         # win32print.WritePrinter(prt, img)
         # win32print.EndDocPrinter(prt)
